40be464f85338d183c1b0e758064e299ab6df416
Chris <kjman26@gmail.com>
2022-10-26 09:40:54 -0700

    added in case and switch

--- index.js
+++ index.js
@@ -2,34 +2,53 @@ const generateHtml = require('../Team_Profile_Generator/Develop/util/generateHtm
 const fs = require('fs');
 const inquirer = require('inquirer');
 const teamMembers = [];
-const Employee = require('./Develop/lib/Employee')
-
+const Employee = require('./Develop/lib/Employee');
+const Manager = require('./Develop/lib/Manager');
+const Intern = require('./Develop/lib/Intern');
+const Engineer = require('./Develop/lib/Engineer')
+const generateTeam = require('./Develop/util/generateHtml');
+module.exports = teamMembers;
 
 //create a prompt that asks for user input and then takes that input and exports to generateHtml, run through the functions with user created parameters (aka user input) and then import from generatehtml
 
-inquirer.prompt() ([
+const firstPrompt = () => {
+    inquirer.prompt([
     {
-         message: "What is your name?",
-         type: "input",
-         name: "NAME",
+         
+        message: "How would you like to proceed?",
+        type: "list",
+        choices: ['add Manager', 'add Engineer', 'add Intern', 'I am done'],
+        name: "CHOOSE",
     },
-    {
-        message: "What is your email?",
-        type: "input",
-        name: "EMAIL",
-    },
+ ])
 
-])
- .then(ans=>{
+ //first prompted to select a course of action and now creating the switch case depending on what the user selects. Will add in the functions called here, below
+ .then(chooseOption => {
+    switch (chooseOption.CHOOSE) {
+        case 'add Manager':
+            addManager();
+            break;
+        case 'add Engineer':
+            addEngineer();
+            break;
+        case 'add Intern':
+            addIntern();
+        default:
+            writeFile(generateTeam(teamMembers));
+            console.log('team built!')
+
+    }
     console.log(ans)
 })
 
-
-
+const addManager = () => {
+    
+}
 
 const writeFile = (data) => {
-    fs.writeFile('./yourTeam.html', data, (err) =>
-        err ? console.error(err) : console.log('file written successfully'))
+    fs.writeFile('./theTeam.html', data, (err) =>
+        err ? console.error(err) : console.log('File successfully written!'))
 };
 
 writeFile();
+
